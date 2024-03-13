d22033688b179f27cc57b2f0c7df61f59f1a7584
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-03-13 22:12:22 +0100

    Overrided RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,9 +9,43 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name): base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        { 
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var topTwentyPercent = (int)Math.Ceiling(Students.Count * 0.2);
+
+            List<double> grades = Students.OrderByDescending(s => s.AverageGrade).Select(s=>s.AverageGrade).ToList();
+
+            if (grades[topTwentyPercent - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(topTwentyPercent * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(topTwentyPercent * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(topTwentyPercent * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
     }
 }
