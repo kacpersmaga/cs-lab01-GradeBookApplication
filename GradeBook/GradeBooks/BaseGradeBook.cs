e433f6e438d99f6fc88d796e38a1d7f32b51689c
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-03-19 00:54:39 +0100

    Refactored constructor of BaseGradeBook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,13 +15,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
-        public BaseGradeBook(string name)
+        public bool IsWeighted { get; set; }
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
         }
-        
-        public bool IsWeighted { get; set; }
 
         public void AddStudent(Student student)
         {
